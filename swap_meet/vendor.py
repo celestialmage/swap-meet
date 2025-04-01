6da8450365a3a31de1ddf43b167c71ec4c289cda
Olya Mateshov <olyamateshov@gmail.com>
2025-04-01 15:39:38 -0400

    Completed Wave_1 and tests for Wave_1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,16 @@
 class Vendor:
-    pass
+    
+    def __init__(self, inventory=None):
+        self.inventory = [] if inventory == None else inventory 
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        result = False
+        if item in self.inventory:
+            self.inventory.remove(item)
+            result = item
+        return result
+    
